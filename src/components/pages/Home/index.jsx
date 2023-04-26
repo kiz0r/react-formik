4c865208029a76665dbc4f2a1e733a7c7b292849
kiz0r <kkhomenko10@gmail.com>
2023-04-26 16:22:22 +0200

    make pages more responsive and change HomePage messagestyles

--- src/components/pages/Home/index.jsx
+++ src/components/pages/Home/index.jsx
@@ -1,12 +1,16 @@
 import Layout from '../../Layout';
 
 const Home = () => {
+  const messageStyles = {
+    fontSize: '24px',
+    fontWeight: '500',
+  };
   return (
-    <>
-      <Layout>
-        <div>To get started you need to login to your account</div>
-      </Layout>
-    </>
+    <Layout>
+      <div style={messageStyles}>
+        To get started you need to login to your account
+      </div>
+    </Layout>
   );
 };
 
